1d4553030ae952461a3e3952afd1a986bc530fa6
Daniel Ale <denielale9291@gmail.com>
2019-04-18 09:41:41 +0100

    Add resourses and Congigure Blueprint

--- resources/courses.py
+++ resources/courses.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-from flask_restful import Resource
+from flask import jsonify, Blueprint
+from flask_restful import Resource, Api
 import models
 
 
@@ -17,3 +17,9 @@ class Course(Resource):
 
     def delete(self, id):
         return jsonify({{"title": "Python Basics"}})
+
+
+courses_api = Blueprint("resources.courses", __name__)
+api = Api(courses_api)
+api.add_resource(CourseList, "/api/v1/courses", endpoint="courses")
+api.add_resource(Course, "/api/v1/courses/<int:id>", endpoint="course")
--- resources/reviews.py
+++ resources/reviews.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-from flask_restful import Resource
+from flask import jsonify, Blueprint
+from flask_restful import Resource, Api
 import models
 
 
@@ -17,3 +17,9 @@ class Review(Resource):
 
     def delete(self, id):
         return jsonify({{"course": 1, "rating": 5}})
+
+
+reviews_api = Blueprint("resources.reviews", __name__)
+api = Api(reviews_api)
+api.resource(ReviewList, "/reviews", endpoint="reviews")
+api.add_resource(Review, "/reviews/<int:id>", endpoint="review")
